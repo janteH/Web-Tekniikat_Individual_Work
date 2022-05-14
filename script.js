57d9d13603862e1d5565d36ff54ce6cd1cb7dd1b
Jante <jante.wallin@gmail.com>
2022-05-14 22:44:21 +0300

    weather api added

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/**
- fetch('https://yesno.wtf/api/')
-    .then(response => response.json())
-    .then(
-        data => {
-            document.getElementById('print-here').innerHTML = "<h2>" + data.answer + "</h2>";
-            document.getElementById('print-here').innerHTML += '<img src="' + data.image + '"/>';
-        }
-    );
- */
 
 function facebook() {
     window.open('https://www.facebook.com/', '_blank');
--- yhteystiedot/yhteystiedot.js
+++ yhteystiedot/yhteystiedot.js
@@ -5,3 +5,12 @@ function facebook() {
 function instagram() {
     window.open('https://www.instagram.com/', '_blank');
 }
+
+fetch("https://weatherdbi.herokuapp.com/data/weather/helsinki")
+    .then(res=>res.json())
+    .then(
+        data => {
+            document.getElementById('printApi').innerHTML = "<h2>" + data.region + "</h2>";
+            document.getElementById('printApi').innerHTML += data.currentConditions.temp.c + " C " + '<img src="' + data.currentConditions.iconURL + '"/>';
+        }
+    )
