1761c6bed127ccfa9d7c179065689848cf9acabc
Jante <jante.wallin@gmail.com>
2022-05-15 14:08:58 +0300

    testing api

--- yhteystiedot/yhteystiedot.js
+++ yhteystiedot/yhteystiedot.js
@@ -6,6 +6,7 @@ function instagram() {
     window.open('https://www.instagram.com/', '_blank');
 }
 
+/*
 fetch("http://api.weatherapi.com/v1/current.json?key=eee59b26e75b400882f91836221505&q=helsinki&aqi=no")
     .then(res=>res.json())
     .then(
@@ -14,3 +15,4 @@ fetch("http://api.weatherapi.com/v1/current.json?key=eee59b26e75b400882f91836221
             document.getElementById('printApi').innerHTML += data.current.temp_c + " °C " + '<img src="https:' + data.current.condition.icon + '"/>';
         }
     )
+*/
